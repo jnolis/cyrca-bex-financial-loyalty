d74257be2de066bcba4ac2a29d1700318be1bf21
jnolis <jnolis@users.noreply.github.com>
2025-10-15 04:55:47 +0000

    improved LOB modeling

--- financial-model-rstats/model.R
+++ financial-model-rstats/model.R
@@ -32,6 +32,7 @@ if(is_testing){
     sample_n(1000) |>
     inner_join(data$tx, join_by(ccid))
 } else {
+  message("Using all transactions")
   tx_raw <- data$tx
 }
 
@@ -41,7 +42,11 @@ if(is_testing){
 tx <- 
   tx_raw |>
   inner_join(segments, join_by(ccid)) |>
-  mutate(lob_ext = if_else(package_indicator > 0,glue("package_{lob}"),lob)) |>
+  mutate(lob_ext = 
+           if_else(package_indicator > 0,glue("package_{lob}"),lob) |>
+           factor()
+           ) |>
+  rename(gbvlc = gbv) |>
   filter(unit_count > 0)
 
 # the year we are going to analyze the transactions
@@ -65,16 +70,21 @@ current_year_units <-
   summarize(units_earned_current_year = sum(unit_count)) |>
   as_tibble()
 
-# what is the LOB of each member
-booking_lob <- 
+# for each booking get the LOB gbv and is_lob (to count the bookings)
+bookings_lob <- 
   tx |>
   lazy_dt() |>
   group_by(booking_code, lob_ext) |>
-  summarize(gbv = sum(gbv),
-            .groups = "drop_last") |>
-  mutate(prct_lob = gbv/sum(gbv)) |>
-  select(booking_code, lob_ext, prct_lob) |>
-  pivot_wider(names_from = lob_ext, names_prefix = "lob_", values_from = prct_lob, values_fill = 0) |>
+  summarize(gbvlc = sum(gbvlc),
+            bookings = 1,
+            .groups = "drop") |>
+  mutate(temp_lob_ext = lob_ext) |>
+  pivot_wider(names_from = temp_lob_ext, names_prefix = "lob_gbvlc_", values_from = gbvlc, values_fill = 0, names_sort = TRUE) |>
+  mutate(temp_lob_ext = lob_ext) |>
+  pivot_wider(names_from = temp_lob_ext, names_prefix = "lob_num_bookings_", values_from = bookings, values_fill = 0, names_sort = TRUE) |>
+  select(-lob_ext) |>
+  group_by(booking_code) |>
+  summarize(across(starts_with("lob_"),sum)) |>
   as_tibble()
 
 
@@ -85,61 +95,89 @@ max_units_earned_group <- 60
 unit_calculation_data <-
   tx |>
   lazy_dt() |>
+  # get just the current year and valid bookings
   filter(year(begin_use_date) == analysis_year, unit_count > 0) |>
+  
+  # get a row for each booking
   group_by(ccid, booking_code, segment) |>
   summarize(unit_count = sum(unit_count),
             begin_use_date = min(begin_use_date),
-            gbv = sum(gbv),
+            loyalty_redemption = sum(loyalty_amount_net_burned_usd),
+            gbvlc = sum(gbvlc),
          .groups="drop") |>
+  
+  # make the units increment across the year
   group_by(ccid) |>
   arrange(begin_use_date) |>
   mutate(units_earned_start_of_booking = lag(cumsum(unit_count), default = 0)) |>
+  
+  # join to the customer attributes
   left_join(prev_year_units, join_by(ccid)) |>
   left_join(current_year_units, join_by(ccid)) |>
+  
+  # fill zeros and cap the upper bounds
   mutate(across(starts_with("units_earned_"),~replace_na(.x,0))) |>
   mutate(across(starts_with("units_earned_"),~if_else(.x>max_units_earned_group,max_units_earned_group,.x))) |>
+  
+  # get the LOB attributes
+  left_join(bookings_lob, join_by(booking_code)) |>
   as_tibble()
-
-
-# LOB BIZ
-
-aggregated_lob <- unit_calculation_data |>
-  select(units_earned_current_year,units_earned_prev_year,segment, booking_code, gbv) |>
-  inner_join(booking_lob, join_by(booking_code)) |>
-  group_by(
-    segment, units_earned_current_year, units_earned_prev_year
-  ) |>
-  summarize(across(starts_with("lob_"), ~sum(.x*gbv,na.rm=T)),.groups="drop")
-
 
 # OUTPUT
 
 aggregated_bookings_for_simulator <-
   unit_calculation_data |>
   group_by(units_earned_start_of_booking, units_earned_prev_year, segment) |>
-  summarize(gbv = sum(gbv),
+  summarize(gbvlc = sum(gbvlc),
             num_bookings = n(),
+            loyalty_redemption = sum(loyalty_redemption),
+            across(starts_with("lob_"),sum),
             .groups = "drop") |>
-  mutate(across(c(gbv, num_bookings), ~.x / ex_frac))
-
-
+  mutate(gbvlc_per_booking = gbvlc / num_bookings) |>
+  mutate(across(starts_with("lob_gbvlc_"),~.x/gbvlc)) |>
+  mutate(across(starts_with("lob_num_bookings_"),~.x/num_bookings)) |>
+  mutate(across(c(gbvlc, num_bookings, loyalty_redemption), ~.x / ex_frac)) |>
+  select(units_earned_start_of_booking,
+         units_earned_prev_year,
+         segment,
+         gbvlc,
+         num_bookings,
+         loyalty_redemption,
+         gbvlc_per_booking,
+         starts_with("lob_"))
+  
 aggregated_members_for_simulator <-
   unit_calculation_data |>
   group_by(segment, units_earned_prev_year, units_earned_current_year) |>
   summarize(num_members = n_distinct(ccid),
             num_bookings = n(),
-            total_gbv = sum(gbv),
+            gbvlc = sum(gbvlc),
+            loyalty_redemption = sum(loyalty_redemption),
             num_units = sum(unit_count),
-            across(starts_with("lob_"),~sum(.x,na.rm=T)),
+            across(starts_with("lob_"),sum),
             .groups="drop") |>
-  mutate(across(c(num_members, num_bookings, total_gbv, num_units), ~.x/ex_frac)) |>
   mutate(units_per_booking = num_units/num_bookings,
-         bookings_per_member = num_bookings/num_members) |>
-  left_join(aggregated_lob, join_by(segment, units_earned_current_year, units_earned_prev_year)) |>
-  rowwise() |>
-  mutate(across(starts_with("lob_"),~.x/sum(c_across(starts_with("lob_"))))) |>
-  ungroup()
+         bookings_per_member = num_bookings/num_members,
+         gbvlc_per_booking = gbvlc / num_bookings,
+         gbvlc_per_member = gbvlc / num_members) |>
+  mutate(across(starts_with("lob_gbvlc_"),~.x/gbvlc)) |>
+  mutate(across(starts_with("lob_num_bookings_"),~.x/num_bookings)) |>
+  mutate(across(c(num_members, num_bookings, gbvlc, num_units, loyalty_redemption), ~.x/ex_frac)) |>
+    select(units_earned_prev_year,
+           units_earned_current_year,
+           segment,
+           gbvlc,
+           num_bookings,
+           loyalty_redemption,
+           num_units,
+           num_members,
+           gbvlc_per_booking,
+           gbvlc_per_member,
+           units_per_booking,
+           bookings_per_member,
+           starts_with("lob_"))
+
 
 fs::dir_create(glue("{output_folder}/{run_id}"))
-write_csv(aggregated_bookings_for_simulator,glue("{output_folder}/{run_id}/bookings.csv"))
-write_csv(aggregated_members_for_simulator,glue("{output_folder}/{run_id}/members.csv"))
+write_csv(aggregated_bookings_for_simulator, glue("{output_folder}/{run_id}/bookings.csv") )
+write_csv(aggregated_members_for_simulator,  glue("{output_folder}/{run_id}/members.csv")  )
